97485f78b831341558e46e73e4b9f3d6c985785f
Dmitry Misevra <shumi199@yandex.ru>
2024-02-28 02:46:07 +0600

    fix: поправить schema.sql

--- main-service/src/main/resources/schema.sql
+++ main-service/src/main/resources/schema.sql
@@ -19,20 +19,20 @@
 -- delete
 -- from categories;
 
-alter table compilations
-    alter column compilation_id restart with 1;
-
-alter table users
-    alter column user_id restart with 1;
-
-alter table categories
-    alter column category_id restart with 1;
-
-alter table events
-    alter column event_id restart with 1;
-
-alter table requests
-    alter column request_id restart with 1;
+-- alter table compilations
+--     alter column compilation_id restart with 1;
+--
+-- alter table users
+--     alter column user_id restart with 1;
+--
+-- alter table categories
+--     alter column category_id restart with 1;
+--
+-- alter table events
+--     alter column event_id restart with 1;
+--
+-- alter table requests
+--     alter column request_id restart with 1;
 
 
 create table if not exists public.users
